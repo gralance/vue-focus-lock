a73647cea2a3cf8661dec52e234a9c13af89df9f
Lance Cheng <lancecheng@neptuneswap.com>
2017-12-14 15:11:42 +0800

    Upgrade

--- dist/index.js
+++ dist/index.js
@@ -731,7 +731,7 @@ exports.default = pickFirstFocus;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = ['button:enabled', 'select:enabled', 'textarea:enabled', 'input:enabled', 'a[href]', 'area[href]', 'iframe', 'object', 'embed', '[tabindex]', '[contenteditable]', '[autofocus]'];
+exports.default = ['button:enabled', 'select:enabled', 'textarea:enabled', 'input:enabled', '.el-select-dropdown__item', 'a[href]', 'area[href]', 'iframe', 'object', 'embed', '[tabindex]', '[contenteditable]', '[autofocus]'];
 
 /***/ }),
 /* 13 */
